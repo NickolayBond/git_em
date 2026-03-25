505e86157dc4173cd1691b40a69f51b0222cf572
NickolayBond <115872317+NickolayBond@users.noreply.github.com>
2026-03-25 18:04:04 +0300

    Выполнил урок 4

--- lesson4/Task1.js
+++ lesson4/Task1.js
@@ -1,4 +1,8 @@
-const obj = {}
-let isEmppty = false;
-...
-console.log(isEmpty)
+/* Нужно написать код, который проверит, является ли объект пустым. */
+
+const obj = {};
+let isEmpty = false;
+
+if (!Object.keys(obj).length) isEmpty = true;
+
+console.log(isEmpty);
--- lesson4/Task2.js
+++ lesson4/Task2.js
@@ -1,4 +1,10 @@
-const obj = { prop1: 1, prop2: 2, prop3: true, prop4: 'test', prop5: 10 }
-let resultObj = {}
-...
+/* Нужно написать код, который оставит в объекте только свойства, которые являются четными числами. Исходные объект изменять нельзя: */
+
+const obj = { prop1: 1, prop2: 2, prop3: true, prop4: "test", prop5: 10 };
+let resultObj = {};
+
+for (let key in obj) {
+    if (typeof obj[key] === "number" && obj[key] % 2 === 0) resultObj[key] = obj[key];
+}
+
 console.log(resultObj); // { prop2: 2, prop5: 10 }
